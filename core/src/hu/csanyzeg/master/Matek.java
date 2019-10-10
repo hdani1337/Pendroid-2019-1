695ab2c3d0515f239c7d237ffa4c2b056c5420ce
zltmmikls10 <zltmmikls10@gmail.com>
2019-10-10 19:22:04 +0200

    Minimális vízszámító mechanika és getterek és settek implementálása, több-kevesebb sikerrel

--- core/src/hu/csanyzeg/master/Matek.java
+++ core/src/hu/csanyzeg/master/Matek.java
@@ -2,36 +2,62 @@ package hu.csanyzeg.master;
 
 import java.util.ArrayList;
 
+
 public class Matek {
 
     float beviz;
     float time = 0;
-    float kimeno ;
-    float vizmennyiseg = 10;//m3
+    float kimeno = 1;
+    float vizmennyiseg = 10;//m3 900 az optimális, a teljes vizmennyiség megkapásához hozzáadunk 890m3-t
     ArrayList<cso> pipe = new ArrayList<cso>();
 
-    public Matek(float vizbe, float[] kimeno) {
+    public Matek(float vizbe, float[] csok) {
         beviz = vizbe;
-        for (float a : kimeno){
-            cso asd = new cso(a);
+        int co = 0;
+        for (float a : csok){
+            co++; //kell hogy tudjuk melyik cső lesz, lusta voltam for-ba
+            cso asd = new cso(a,co);
+            pipe.add(asd);
         }
+    }
+
+    public ArrayList<cso> getPipe() {
+        return pipe;
+    }
+
+    public cso getcso(int i) {
+        return getPipe().get(i);
     }
 
     public float getVizmennyiseg(){
         return vizmennyiseg;
     }
 
-    public void step(float deltaTime){
+    public void step(float deltaTime)
+    {
         time+=deltaTime;
+        vizmennyiseg += (deltaTime * beviz); //hozzáadjuk az eltelt idő alatt befolyt vizet
+        if (beviz != 0) getcso(0).setOpen(true); //ha van befolyó víz akkor a min kimemet megy, ezt felülírja majd ha lemegy a vízszint 1,25 alá mondjuk a 10-ből
+        if (getVizmennyiseg()> 11) getcso(1).setOpen(true);
+        setKimeno();
+        vizmennyiseg += -1*(deltaTime*kimeno); // kivonjuk az eltelt idő alatt kifolyt vizet
+        // Fontos hogy a vízmennyiség nem mehet 890 m3 alá, de 910m3 fölé sem! tehát van egy kellemes 20m3-nyi területünk floatban
+
     }
-/*
-    public static float mennyiseg() {
-        float valtozas = beviz * getTime - (kimeno * time);
-        float sum = 900 + valtozas;
+
+    public void setKimeno() {
+        float sum =0; //csak egy void ami összegzi a nyílt csöveken átmenő vízmennyiséget
+        for (int i = 0; i < getPipe().size(); i++)
+        {
+            if(getcso(i).isOpen()) sum+=getcso(i).getKi();
+        }
+        this.kimeno = sum;
     }
-*/
+
     public static void main(String[] args) {
-        Matek m = new  Matek(2, new float[] {2,3, 1});
+        float[] csovek = {1,2,4,8};
+        Matek m = new  Matek(2, csovek);
+        System.out.println(m.getVizmennyiseg());
         for(int i = 0; i< 20; i++){
             m.step(0.1f);
             System.out.println(m.getVizmennyiseg());
--- core/src/hu/csanyzeg/master/cso.java
+++ core/src/hu/csanyzeg/master/cso.java
@@ -2,13 +2,53 @@ package hu.csanyzeg.master;
 
 public class cso {
 
-    float ki;
 
-    public cso(float asd) {
+    int key = 0;
+    float ki = 0;
+    boolean isOpen = false;
+
+    public cso(float asd, int k) {
         ki = asd;
+        key = k;
+        main(new String[]{"none"});
     }
 
-    public static void main(String[] args) {
-        System.out.println("szoszi");
+    public Float getKi() {
+        return this.ki;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public String getKistring() {
+        String s = Float.toString(getKi());
+        return s;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        if(open){
+            if (!isOpen){
+                isOpen = true;
+                System.out.println(getKey() + "-as cso kinyílt");
+            }
+        }
+        else {
+            if(isOpen){
+                isOpen = false;
+                System.out.println(getKey() + "-as cso záródott");
+            }
+        }
+
+
+    }
+
+    public void main(String[] args) {
+      //  System.out.println("szoszi");
+        System.out.printf("Cső létrejött és %s vizet enged át%n", getKistring());
     }
 }
