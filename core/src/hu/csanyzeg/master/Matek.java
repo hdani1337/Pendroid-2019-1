f6e93d3021dcd198ee853e4df0da52fa578e1c99
zltmmikls10 <zltmmikls10@gmail.com>
2019-10-16 15:38:23 +0200

    Matek merge, kész ha nem baszakodunk azzal hogy menet közben is lehet csövet hozzáadni

--- core/src/hu/csanyzeg/master/Matek.java
+++ core/src/hu/csanyzeg/master/Matek.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 
 public class Matek {
 
-    float beviz =  0;
+    float beviz;
     float time = 0;
     float kimeno = 1;
-    float vizmennyiseg = 10;//m3 900 az optimális, a teljes vizmennyiség megkapásához hozzáadunk 890m3-t
     float difi = 0;
+    float vizmennyiseg = 10;//m3 900 az optimális, a teljes vizmennyiség megkapásához hozzáadunk 890m3-t
     ArrayList<cso> pipe = new ArrayList<cso>();
     ArrayList<Float> szintek = new ArrayList<Float>();
 
+
     public Matek(float vizbe, float[] csok) {
         beviz = vizbe;
-        int counter = 0;
+        int co = 0;
         for (float a : csok){
-            counter++; //kell hogy tudjuk melyik cső lesz, lusta voltam for-ba
-            cso asd = new cso(a,counter);
+            co++; //kell hogy tudjuk melyik cső lesz, lusta voltam for-ba
+            cso asd = new cso(a,co);
             pipe.add(asd);
         }
         difi = 20 / csok.length;
-
     }
 
     public ArrayList<cso> getPipe() {
@@ -37,9 +37,73 @@ public class Matek {
         return vizmennyiseg;
     }
 
+    public float getOsszesKimeno()
+    {
+        float sum =0; //csak egy void ami visszaadja az összes csövön átmenő vízmennyiséget
+        for (int i = 0; i < getPipe().size(); i++)
+        {
+            sum+=getcso(i).getKi();
+        }
+        return sum;
+    }
+
     public float getKimeno()
     {
         return kimeno;
+    }
+
+    public float getBemeno()
+    {
+        return beviz; //csak egy void ami visszaadja a nyílt csöveken átmenő vízmennyiséget
+    }
+
+    public float getLegkisebb()
+    {
+        float max = getLegnagyobb();
+        for (int i = 0; i < getPipe().size(); i++)
+        {
+            if(getPipe().get(i).getKi() < max) max = getPipe().get(i).getKi();
+        }
+        return max;
+    }
+
+    public float getAtlag()
+    {
+        return getOsszesKimeno()/pipe.size();
+    }
+
+    public float getLegnagyobb()
+    {
+        float min = 0;
+        for (int i = 0; i < getPipe().size(); i++)
+        {
+            if(getPipe().get(i).getKi() > min) min = getPipe().get(i).getKi();
+        }
+        return min;
+    }
+
+    public void step(float deltaTime)
+    {
+        time+=deltaTime;
+        vizmennyiseg += (deltaTime * beviz); //hozzáadjuk az eltelt idő alatt befolyt vizet
+        if (beviz > getLegkisebb()) getcso(0).setOpen(true); //ha több víz folyik be, mint amennyit a legkisebb csap elbír, akkor nyíljon ki az első, mert ha kevesebb víz folyik be mint a legkisebb csap, akkor lemegy 8.9 alá
+        if (getVizmennyiseg()> 11) getcso(1).setOpen(true);
+        setKimeno();
+        vizmennyiseg += -1*(deltaTime*kimeno); // kivonjuk az eltelt idő alatt kifolyt vizet
+        // Fontos hogy a vízmennyiség nem mehet 890 m3 alá, de 910m3 fölé sem! tehát van egy kellemes 20m3-nyi területünk floatban
+
+    }
+
+    public void setKimeno() {
+        float sum =0; //csak egy void ami összegzi a nyílt csöveken átmenő vízmennyiséget
+        for (int i = 0; i < pipe.size(); i++){
+            if (getVizmennyiseg() > szintek.get(i)) getcso(i).setOpen(true);
+            else getcso(i).setOpen(false);
+        }
+        for (int i = 0; i < pipe.size(); i++) {
+            if (getcso(i).isOpen()) sum += getcso(i).getKi();
+        }
+        this.kimeno = sum;
     }
 
     public void szintfeltoltes(){
@@ -50,47 +114,9 @@ public class Matek {
         }
     }
 
-    public void step(float deltaTime)
+    public void setBemeno(float vizbe)
     {
-        time+=deltaTime;
-        vizmennyiseg += (deltaTime * beviz); //hozzáadjuk az eltelt idő alatt befolyt vizet
-        if (beviz != 0) getcso(0).setOpen(true); //ha van befolyó víz akkor a min kimemet megy, ezt felülírja majd ha lemegy a vízszint 1,25 alá mondjuk a 10-ből
-        if (getVizmennyiseg()> 11) getcso(1).setOpen(true);
-        setKimeno();
-        vizmennyiseg += -1*(deltaTime*kimeno); // kivonjuk az eltelt idő alatt kifolyt vizet
-        // Fontos hogy a vízmennyiség nem mehet 890 m3 alá, de 910m3 fölé sem! tehát van egy kellemes 20m3-nyi területünk floatban
-
-    }
-
-    public float getBeviz() {
-        return beviz;
-    }
-
-    public float getDifi() {
-        return difi;
-    }
-
-    public float getLegkisebb(){
-        float min = 32768;
-        for (int i = 0; i < getPipe().size(); i++){
-            if (getPipe().get(i).getKi() < min ) min = getPipe().get(i).getKi();
-        }
-        return min;
-    }
-
-
-    public void setKimeno() {
-        float sum =0; //csak egy void ami összegzi a nyílt csöveken átmenő vízmennyiséget
-        for (int i = 0; i < pipe.size(); i++){
-            if (getVizmennyiseg() > szintek.get(i)) getcso(i).setOpen(true);
-            else getcso(i).setOpen(false);
-        }
-
-
-        for (int i = 0; i < pipe.size(); i++) {
-            if (getcso(i).isOpen()) sum += getcso(i).getKi();
-        }
-        this.kimeno = sum;
+        beviz = vizbe;
     }
 
     public static void main(String[] args) {
